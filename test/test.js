f6445a68e200411f45663d2c9cb04eaff071fb94
june_01 <june_01@163.com>
2017-07-21 11:34:31 +0800

    Rm proxy

--- test/test.js
+++ test/test.js
@@ -6,5 +6,4 @@ const comicSpider = require('../index');
 comicSpider({
     sourceUrl: ['http://manhua.dmzj.com/mingrijiangdeshuishoufu', 'http://manhua.dmzj.com/qiguaidejiejie'],
     dirPath: path.join(__dirname, './'),
-    proxy: 'http://dev-proxy.oa.com:8080',
 });
--- test/testCli.js
+++ test/testCli.js
@@ -5,6 +5,5 @@ const Args = require('../src/args');
 
 const sourceUrl = ['http://manhua.dmzj.com/mingrijiangdeshuishoufu', 'http://manhua.dmzj.com/qiguaidejiejie'];
 const dirPath = path.join(__dirname, './');
-const proxy = 'http://dev-proxy.oa.com:8080';
 
-new Args([...sourceUrl, '-o', dirPath, '-p', proxy]);
+new Args([...sourceUrl, '-o', dirPath]);
